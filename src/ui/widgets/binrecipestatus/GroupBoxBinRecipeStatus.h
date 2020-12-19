204f29c781fd89893da935e96331906fe420fbfb
Ghettovich <mrfunkyman@pm.me>
2020-12-19 02:02:24 +0100

    added recipe data subscription for group box recipe status.

--- src/ui/widgets/binrecipestatus/GroupBoxBinRecipeStatus.h
+++ src/ui/widgets/binrecipestatus/GroupBoxBinRecipeStatus.h
@@ -5,21 +5,39 @@ namespace Ui {
     class GroupBoxBinRecipeStatus;
 }
 
+#include <iodevice.h>
+#include <widgets/interfaces/RecipeStatusInterface.h>
+#include <recipe.h>
 #include <MqttClient.h>
+#include <fonts/MaterialRegular.h>
 #include <QObject>
 #include <QtWidgets/QWidget>
+#include <QTableWidgetItem>
 
-class GroupBoxBinRecipeStatus : public QWidget {
+class GroupBoxBinRecipeStatus : public RecipeStatusInterface {
 
     Q_OBJECT
+    Q_INTERFACES(RecipeStatusInterface)
 
 public:
     explicit GroupBoxBinRecipeStatus(MqttClient *m_client);
     virtual ~GroupBoxBinRecipeStatus();
 
+public slots:
+    void onUpdateIODevice(const WeightSensor *sensor) override;
+
 private:
     Ui::GroupBoxBinRecipeStatus *ui = nullptr;
     MqttClient *m_client = nullptr;
+    IODevice *scale1 = nullptr;
+    //IODevice *scaleBin = nullptr;
+    Recipe selectedRecipe = Recipe(0);
+    Component activeComponent = Component(0);
+    QTableWidgetItem *activeComponentTableWidget = nullptr;
+
+    void createRecipeComponentTableWidget();
+    void updateComponentWidgetTable();
+    void setQLcdNumberDisplay();
 };
 
 
