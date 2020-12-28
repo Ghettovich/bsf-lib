06b6bcf5e2753fc002af30647171a7226e0b0b03
Ghettovich <mrfunkyman@pm.me>
2020-12-28 03:55:04 +0100

    should pass pointer.

--- test/ui/widgets/tst_GroupBoxBinLoadDrop.cpp
+++ test/ui/widgets/tst_GroupBoxBinLoadDrop.cpp
@@ -11,20 +11,20 @@ void GroupBoxBinLoadDropTest::initTestCase() {
 }
 
 void GroupBoxBinLoadDropTest::testCustomSignal() {
-  auto parent = new QObject;
-  MqttClient object(parent);
-  auto mo = object.metaObject();
+  //auto parent = new QObject;
+  auto object = new MqttClient(this);
+  auto mo = object->metaObject();
 
   qRegisterMetaType<QVector<IODevice *>>();
   auto signalIndex = mo->indexOfSignal("newIODeviceStates(QVector<IODevice*>)");
   auto signal = mo->method(signalIndex);
 
-  QSignalSpy spy(&object, signal);
+  QSignalSpy spy(object, signal);
   QMqttTopicName topic("/relay/states");
 
   QFile jsonFile(":/payload/relayStates.json");
   jsonFile.open(QIODevice::ReadOnly);
-  object.onMessageReceived(jsonFile.readAll(), topic);
+  object->onMessageReceived(jsonFile.readAll(), topic);
   QCOMPARE(spy.count(), 1);
 
   QList<QVariant> arguments = spy.takeFirst();
